03fd5d95a6d8761408976cff1636be0f8047ffa4
str4d <str4d@mail.i2p>
2017-12-07 13:57:57 +1300

    Migrate to James Server 3.0.1

--- webapp/src/main/java/i2p/bote/imap/ImapService.java
+++ webapp/src/main/java/i2p/bote/imap/ImapService.java
@@ -21,11 +21,40 @@
 
 package i2p.bote.imap;
 
-import i2p.bote.Configuration;
-import i2p.bote.fileencryption.PasswordException;
-import i2p.bote.fileencryption.PasswordVerifier;
-import i2p.bote.folder.EmailFolder;
-import i2p.bote.folder.EmailFolderManager;
+import net.i2p.util.Log;
+import net.i2p.util.StrongTls;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.james.filesystem.api.FileSystem;
+import org.apache.james.imap.api.process.ImapProcessor;
+import org.apache.james.imap.decode.ImapDecoder;
+import org.apache.james.imap.encode.ImapEncoder;
+import org.apache.james.imap.encode.main.DefaultImapEncoderFactory;
+import org.apache.james.imap.encode.main.DefaultLocalizer;
+import org.apache.james.imap.main.DefaultImapDecoderFactory;
+import org.apache.james.imap.processor.main.DefaultImapProcessorFactory;
+import org.apache.james.imapserver.netty.IMAPServer;
+import org.apache.james.imapserver.netty.ImapMetrics;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.SubscriptionManager;
+import org.apache.james.mailbox.acl.GroupMembershipResolver;
+import org.apache.james.mailbox.acl.MailboxACLResolver;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.exception.SubscriptionException;
+import org.apache.james.mailbox.exception.UnsupportedRightException;
+import org.apache.james.mailbox.model.MailboxACL;
+import org.apache.james.mailbox.model.MailboxACL.MailboxACLEntryKey;
+import org.apache.james.mailbox.model.MailboxACL.MailboxACLRight;
+import org.apache.james.mailbox.model.MailboxACL.MailboxACLRights;
+import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.model.SimpleMailboxACL;
+import org.apache.james.mailbox.store.Authenticator;
+import org.apache.james.mailbox.store.Authorizator;
+import org.apache.james.mailbox.store.RandomMailboxSessionIdGenerator;
+import org.apache.james.mailbox.store.StoreMailboxManager;
+import org.apache.james.mailbox.store.mail.model.impl.MessageParser;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -39,36 +68,11 @@ import javax.mail.Flags;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
-import net.i2p.util.Log;
-import net.i2p.util.StrongTls;
-
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.HierarchicalConfiguration;
-import org.apache.james.filesystem.api.FileSystem;
-import org.apache.james.imap.api.display.HumanReadableText;
-import org.apache.james.imap.api.display.Locales;
-import org.apache.james.imap.api.display.Localizer;
-import org.apache.james.imap.api.process.ImapProcessor;
-import org.apache.james.imap.encode.main.DefaultImapEncoderFactory;
-import org.apache.james.imap.main.DefaultImapDecoderFactory;
-import org.apache.james.imap.processor.main.DefaultImapProcessorFactory;
-import org.apache.james.imapserver.netty.IMAPServer;
-import org.apache.james.mailbox.MailboxSession;
-import org.apache.james.mailbox.SubscriptionManager;
-import org.apache.james.mailbox.acl.GroupMembershipResolver;
-import org.apache.james.mailbox.acl.MailboxACLResolver;
-import org.apache.james.mailbox.exception.SubscriptionException;
-import org.apache.james.mailbox.exception.UnsupportedRightException;
-import org.apache.james.mailbox.model.MailboxACL;
-import org.apache.james.mailbox.model.MailboxACL.MailboxACLEntryKey;
-import org.apache.james.mailbox.model.MailboxACL.MailboxACLRight;
-import org.apache.james.mailbox.model.MailboxACL.MailboxACLRights;
-import org.apache.james.mailbox.model.SimpleMailboxACL;
-import org.apache.james.mailbox.store.Authenticator;
-import org.apache.james.mailbox.store.HashMapDelegatingMailboxListener;
-import org.apache.james.mailbox.store.RandomMailboxSessionIdGenerator;
-import org.apache.james.mailbox.store.StoreMailboxManager;
-import org.slf4j.LoggerFactory;
+import i2p.bote.Configuration;
+import i2p.bote.fileencryption.PasswordException;
+import i2p.bote.fileencryption.PasswordVerifier;
+import i2p.bote.folder.EmailFolder;
+import i2p.bote.folder.EmailFolderManager;
 
 /**
  * IMAP implementation for I2P-Bote using <a href="http://james.apache.org/">
@@ -79,12 +83,59 @@ public class ImapService extends IMAPServer {
     private static final String SSL_KEYSTORE_FILE = "file:///BoteSSLKeyStore";
     
     private Log log = new Log(ImapService.class);
-    private EmailFolderManager folderManager;
     private File sslKeyStore;
     private MapperFactory mailboxSessionMapperFactory;
 
-    public ImapService(Configuration configuration, final PasswordVerifier passwordVerifier, EmailFolderManager folderManager) throws ConfigurationException {
-        this.folderManager = folderManager;
+    public static ImapService create(Configuration configuration,
+                                     final PasswordVerifier passwordVerifier,
+                                     EmailFolderManager folderManager) throws ConfigurationException {
+        MapperFactory mailboxSessionMapperFactory = new MapperFactory(folderManager);
+        Authenticator authenticator = createAuthenticator(passwordVerifier);
+        Authorizator authorizator = new Authorizator() {
+            @Override
+            public AuthorizationState canLoginAsOtherUser(String userId, String otherUserId) throws MailboxException {
+                return AuthorizationState.UNKNOWN_USER;
+            }
+        };
+        MailboxACLResolver aclResolver = createMailboxACLResolver();
+        GroupMembershipResolver groupMembershipResolver = new GroupMembershipResolver() {
+            public boolean isMember(String user, String group) {
+                return true;
+            }
+        };
+        MessageParser messageParser = new MessageParser();
+        MessageId.Factory messageIdFactory = new BoteMessageId.Factory();
+
+        StoreMailboxManager mailboxManager = new StoreMailboxManager(
+                mailboxSessionMapperFactory, authenticator, authorizator,
+                aclResolver, groupMembershipResolver, messageParser, messageIdFactory,
+                0, 0);
+        mailboxManager.setMailboxSessionIdGenerator(new RandomMailboxSessionIdGenerator());
+
+        SubscriptionManager subscriptionManager = createSubscriptionManager(folderManager);
+
+        DefaultImapEncoderFactory encoderFactory = new DefaultImapEncoderFactory(
+                new DefaultLocalizer(), true);
+
+        DefaultImapProcessorFactory processorFactory = new DefaultImapProcessorFactory();
+        processorFactory.setMailboxManager(mailboxManager);
+        processorFactory.setSubscriptionManager(subscriptionManager);
+
+        return new ImapService(
+                DefaultImapDecoderFactory.createDecoder(),
+                encoderFactory.buildImapEncoder(),
+                processorFactory.buildImapProcessor(),
+                null,
+                configuration,
+                mailboxSessionMapperFactory);
+    }
+
+    ImapService(ImapDecoder decoder, ImapEncoder encoder, ImapProcessor processor,
+                ImapMetrics imapMetrics, Configuration configuration,
+                MapperFactory mailboxSessionMapperFactory)
+            throws ConfigurationException {
+        super(decoder, encoder, processor, imapMetrics);
+        this.mailboxSessionMapperFactory = mailboxSessionMapperFactory;
         
         setLog(LoggerFactory.getLogger(ImapService.class));
 
@@ -134,34 +185,10 @@ public class ImapService extends IMAPServer {
                 } catch (IOException e) {}
         }
         configure(cfg);   // use the defaults for the rest
-
-        mailboxSessionMapperFactory = new MapperFactory(folderManager);
-        MailboxACLResolver aclResolver = createMailboxACLResolver();
-        GroupMembershipResolver groupMembershipResolver = new GroupMembershipResolver() {
-            public boolean isMember(String user, String group) {
-                return true;
-            }
-        };
-        Authenticator authenticator = createAuthenticator(passwordVerifier);
-        StoreMailboxManager<String> mailboxManager = new StoreMailboxManager<String>(mailboxSessionMapperFactory, authenticator, aclResolver, groupMembershipResolver);
-        mailboxManager.setDelegatingMailboxListener(new HashMapDelegatingMailboxListener());
-        mailboxManager.setMailboxSessionIdGenerator(new RandomMailboxSessionIdGenerator());
-        
-        SubscriptionManager subscriptionManager = createSubscriptionManager();
-        
-        ImapProcessor processor = DefaultImapProcessorFactory.createDefaultProcessor(mailboxManager, subscriptionManager);
-        setImapProcessor(processor);
-
-        setImapEncoder(DefaultImapEncoderFactory.createDefaultEncoder(new Localizer() {
-            public String localize(HumanReadableText text, Locales locales) {
-                return text.getDefaultValue();
-            }
-        }, true));
-        setImapDecoder(DefaultImapDecoderFactory.createDecoder());
     }
     
     /** Creates a <code>MailboxACLResolver</code> that grants a logged in user full rights to everything */
-    private MailboxACLResolver createMailboxACLResolver() {
+    private static MailboxACLResolver createMailboxACLResolver() {
         return new MailboxACLResolver() {
             
             @Override
@@ -195,7 +222,8 @@ public class ImapService extends IMAPServer {
      * Creates a <code>SubscriptionManager</code> that subscribes to all folders.
      * It does not support unsubscribing.
      */
-    private SubscriptionManager createSubscriptionManager() {
+    private static SubscriptionManager createSubscriptionManager(
+            final EmailFolderManager folderManager) {
         return new SubscriptionManager() {
             
             @Override
@@ -213,7 +241,7 @@ public class ImapService extends IMAPServer {
             @Override
             public Collection<String> subscriptions(MailboxSession session) throws SubscriptionException {
                 Collection<String> folderNames = new ArrayList<String>();
-                for (EmailFolder folder: ImapService.this.folderManager.getEmailFolders())
+                for (EmailFolder folder: folderManager.getEmailFolders())
                     folderNames.add(folder.getName());
                 return folderNames;
             }
@@ -225,9 +253,10 @@ public class ImapService extends IMAPServer {
     }
     
     /** Creates an <code>Authenticator</code> that checks the I2P-Bote password. */
-    private Authenticator createAuthenticator(final PasswordVerifier passwordVerifier) {
+    private static Authenticator createAuthenticator(final PasswordVerifier passwordVerifier) {
         return new Authenticator() {
-            
+            private Log log = new Log(Authenticator.class);
+
             @Override
             public boolean isAuthentic(String userid, CharSequence passwd) {
                 if (!IMAP_USER.equals(userid))
